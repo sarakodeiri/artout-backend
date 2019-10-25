7ec71c93b3febc1d5f41f4487e5ced09b1bcc59e
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-10-25 19:47:24 +0330

    Create user model

--- Artout/user/models.py
+++ Artout/user/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    avatar = models.CharField(blank=True,max_length=2000)
+    id = models.AutoField(primary_key=True)
+    device_token = models.CharField(blank=True,max_length=2000)
