2eb1c430530f13efc03157d988a70d62870f560f
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-10-25 20:00:27 +0330

    Update api urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from Artout.user.views import RegisterView
+from rest_framework_simplejwt import views as jwt_views
+from Artout.user.views import LoginView
 
 url_pattern = [
-
+    path('users/', include('Artout.user.urls')),
+    path('register/',RegisterView.as_view()),
+    path('login/', LoginView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
