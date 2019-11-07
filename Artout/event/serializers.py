15ec4fb19e36131e15ed339f467327a39dfb6c2e
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-11-07 22:40:24 +0330

    Override event serializer create function

--- Artout/event/serializers.py
+++ Artout/event/serializers.py
@@ -9,9 +9,17 @@ class LocationSerializer(serializers.ModelSerializer):
 
 
 class EventSerializer(serializers.ModelSerializer):
+    location = LocationSerializer()
+
     class Meta:
         model = models.Event
         fields = ('title', 'description', 'start_date', 'end_date', 'picture_url', 'event_owner', 'location')
+
+    def create(self, validated_data):
+        location = validated_data.pop('location')
+        event = models.Event.objects.create(**validated_data)
+        models.Location.objects.create(**location)
+        return event
 
 
 class CheckinSerializer(serializers.ModelSerializer):
