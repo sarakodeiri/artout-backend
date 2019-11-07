53c3918178636bc0554475218bab842bcc4101a7
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-11-07 20:31:52 +0330

    Modify EventList post api

--- Artout/event/views.py
+++ Artout/event/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
@@ -10,18 +10,17 @@ from django.http import JsonResponse, HttpResponseBadRequest
 
 class EventList(generics.ListCreateAPIView):
 
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = EventSerializer
 
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-
-        if serializer.is_valid():
-            event = serializer.save()
-            serializer_event = EventSerializer(event)
-            data = {"event": serializer_event.data}
-            return Response(data=data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        user_id = request.data.get('id', None)
+        if user_id is None:
+            return Response("User not found", status=status.HTTP_404_NOT_FOUND)
+        user = UserProfile.objects.get(id = user_id)
+        user_events = Event.objects.filter(event_owner=user)
+        event_ids = [event.id for event in user_events]
+        return Response(event_ids, status=status.HTTP_200_OK)
 
     def get(self, request):
         # is_authenticated = request.user.is_authenticated()
