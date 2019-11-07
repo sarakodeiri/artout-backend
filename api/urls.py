e2515aff2ef0decb91bfdb9e4b5ae3fefa8e699b
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-11-07 15:15:08 +0330

    Add swagger urls

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 from Artout.user.views import RegisterView
 from rest_framework_simplejwt import views as jwt_views
 from Artout.user.views import LoginView
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Artout API')
 
 urlpatterns = [
+    path('docs/', schema_view),
     path('', include('Artout.user.urls')),
     path('register/',RegisterView.as_view()),
     path('login/', LoginView.as_view(), name='token_obtain_pair'),
