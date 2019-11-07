db6a3ca5cae22f2c4117d7bde7bcb5ead1b56a28
Sara Kodeiri <skaordaeiri@gmail.com>
2019-11-07 13:49:48 +0330

    Implement event model.

--- Artout/event/models.py
+++ Artout/event/models.py
@@ -1,5 +1,23 @@
 from django.db import models
+from Artout.user.models import UserProfile
 
-# Create your models here.
+
 class Event(models.Model):
-    
+    id = models.AutoField(primary_key=True)
+    title = models.CharField()
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    rate = models.IntegerField(blank=True, max_length=5)
+    picture = models.CharField(blank=True, max_length=2000)
+    event_owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    location = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True)
+
+
+
+class CheckIn(models.Model):
+    pass
+
+
+class Location(models.Model):
+    pass
