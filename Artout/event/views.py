c996ea09aed2261388d77478bdafc498cd60fe1c
Sara Kodeiri <skaordaeiri@gmail.com>
2019-11-07 19:40:15 +0330

    Make event view headers.

--- Artout/event/views.py
+++ Artout/event/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+
+class EventList(generics.ListCreateAPIView):
+    pass
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    pass
