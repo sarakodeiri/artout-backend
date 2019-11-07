13f605847ded64fb9e172d2b0573a66ad4789b4a
Sara Kodeiri <skaordaeiri@gmail.com>
2019-11-07 17:32:19 +0330

    Register models in admin.py

--- Artout/event/admin.py
+++ Artout/event/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event
+# from .models import Location
+from .models import CheckIn
 
-# Register your models here.
+
+admin.site.register(Event)
+# admin.site.register(Location)
+admin.site.register(CheckIn)
