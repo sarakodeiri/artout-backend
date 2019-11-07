bc2d2263f4f48b97ea288b983552f7c2cbc22028
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-11-07 23:30:22 +0330

    Override event serializer update function

--- Artout/event/serializers.py
+++ Artout/event/serializers.py
@@ -24,6 +24,17 @@ class EventSerializer(serializers.ModelSerializer):
         event.save()
         return event
 
+    def update(self, instance, validated_data):
+        location = validated_data.pop('location')
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.start_date = validated_data.get('start_date', instance.start_date)
+        instance.end_date = validated_data.get('end_date', instance.end_date)
+        instance.picture_url = validated_data.get('picture_url', instance.picture_url)
+        if location is not None:
+            instance.location = Location(longitude=location['longitude'],latitude=location['latitude'])
+        return instance
+
 
 class CheckinSerializer(serializers.ModelSerializer):
     class Meta:
