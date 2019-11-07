ffff93250ace8aea0dbc8e6d88ede69c20e147f9
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-11-07 21:51:35 +0330

    Refactor EventList api

--- Artout/event/views.py
+++ Artout/event/views.py
@@ -5,33 +5,35 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
 from .models import *
+from django.core.exceptions import *
 from django.http import JsonResponse, HttpResponseBadRequest
 
 
 class EventList(generics.ListCreateAPIView):
-
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
+    queryset = Event.objects.all()
     serializer_class = EventSerializer
 
-    def post(self, request, *args, **kwargs):
-        user_id = request.data.get('id', None)
-        if user_id is None:
-            return Response("User not found", status=status.HTTP_404_NOT_FOUND)
-        user = UserProfile.objects.get(id = user_id)
-        user_events = Event.objects.filter(event_owner=user)
-        event_ids = [event.id for event in user_events]
-        return Response(event_ids, status=status.HTTP_200_OK)
+    def get(self, request, *args, **kwargs):
+        user_id = request.GET.get('id', '')
+        if user_id is '':
+            queryset = self.filter_queryset(self.get_queryset())
 
-    def get(self, request):
-        # is_authenticated = request.user.is_authenticated()
-        #
-        # if not is_authenticated:
-        #     return HttpResponseBadRequest()
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
 
-        user_id = 3
-        all_events = Event.objects.filter(event_owner_id=user_id)
-        serializer = EventSerializer(all_events, many=True)
-        return JsonResponse({"all_events": serializer.data})
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            try:
+                user = UserProfile.objects.get(id=user_id)
+                user_events = Event.objects.filter(event_owner=user)
+                event_ids = [event.id for event in user_events]
+                return Response(event_ids, status=status.HTTP_200_OK)
+            except ObjectDoesNotExist:
+                return Response("User not found!", status=status.HTTP_404_NOT_FOUND)
 
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
