baa15fcd1cba5806050eb7ab5091b94230aa0998
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-11-09 02:24:47 +0330

    Add tokens to register serializer

--- Artout/user/serializers.py
+++ Artout/user/serializers.py
@@ -1,5 +1,8 @@
+from django.utils.six import text_type
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from Artout.user import models
 
 
@@ -13,6 +16,17 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.UserProfile
         fields = ('email', 'first_name', 'last_name', 'date_joined', 'avatar', 'username','password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def get_tokens(self, user):
+        tokens = RefreshToken.for_user(user)
+        refresh = text_type(tokens)
+        access = text_type(tokens.access_token)
+        data = {
+            "refresh": refresh,
+            "access": access
+        }
+        return data
 
     def create(self, validated_data):
         return models.UserProfile.objects.create_user(username=validated_data["username"], email=validated_data["email"], password=validated_data["password"],first_name=validated_data["first_name"],last_name=validated_data["last_name"],avatar=validated_data["avatar"])
