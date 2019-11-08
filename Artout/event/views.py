c2513843fe7d5b8540fd20aa0ae955ba8385a0f2
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-11-08 19:52:17 +0330

    Add EventListDetail api to get all the events belonging to a user

--- Artout/event/views.py
+++ Artout/event/views.py
@@ -9,7 +9,7 @@ from django.core.exceptions import *
 from django.http import JsonResponse, HttpResponseBadRequest
 
 
-class EventList(generics.ListCreateAPIView):
+class EventListID(generics.ListCreateAPIView):
     permission_classes = (AllowAny,)
     queryset = Event.objects.all()
     serializer_class = EventSerializer
@@ -32,6 +32,33 @@ class EventList(generics.ListCreateAPIView):
                 user_events = Event.objects.filter(event_owner=user)
                 event_ids = [event.id for event in user_events]
                 return Response(event_ids, status=status.HTTP_200_OK)
+            except ObjectDoesNotExist:
+                return Response("User not found!", status=status.HTTP_404_NOT_FOUND)
+
+
+class EventListDetail(generics.ListCreateAPIView):
+    permission_classes = (AllowAny,)
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+    def get(self, request, *args, **kwargs):
+        user_id = request.GET.get('id', '')
+        if user_id is '':
+            queryset = self.filter_queryset(self.get_queryset())
+
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            try:
+                user = UserProfile.objects.get(id=user_id)
+                user_events = Event.objects.filter(event_owner=user)
+                serializer = self.serializer_class(data=user_events)
+                return Response(serializer.data, status=status.HTTP_200_OK)
             except ObjectDoesNotExist:
                 return Response("User not found!", status=status.HTTP_404_NOT_FOUND)
 
