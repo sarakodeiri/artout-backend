546e8b0eb211eb6d50e1bd29f17030250ef09f89
Mohammad Yahyavi Nasab <yahyavimohammad@gmail.com>
2019-12-05 15:46:27 +0330

    Refactor user model

--- Artout/user/models.py
+++ Artout/user/models.py
@@ -6,5 +6,4 @@ from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
     avatar = models.CharField(blank=True, max_length=2000)
-    id = models.AutoField(primary_key=True)
     device_token = models.CharField(blank=True, max_length=2000)
