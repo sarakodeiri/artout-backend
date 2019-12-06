f95f3fb785a2a4c20df460c09c49e1d96beeefaa
Sara Kodeiri <skaordaeiri@gmail.com>
2019-12-06 19:26:30 +0330

    Modify urls.py in Artout app.

--- Artout/urls.py
+++ Artout/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-
+    path('api/v1.0/events', include('event.urls')),
+    path('api/v1.0/users', include('user.urls'))
 ]
