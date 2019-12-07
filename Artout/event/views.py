002853604a084607da74593dd76143b21ab168a5
Sara Kodeiri <skaordaeiri@gmail.com>
2019-12-07 16:17:32 +0330

    Create create def in event view

--- Artout/event/views.py
+++ Artout/event/views.py
@@ -10,17 +10,27 @@ from django.http import JsonResponse, HttpResponseBadRequest
 
 
 class EventList(generics.ListCreateAPIView):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
     def get_queryset(self):
-        user = get_object_or_404(UserProfile, pk=self.kwargs['id'])
+        user = get_object_or_404(UserProfile, pk=self.kwargs['owner'])
         return Event.objects.filter(owner__id=user.id)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        user = request.user
+        data["owner"] = user
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class EventDetail(generics.RetrieveUpdateAPIView):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = EventSerializer
     queryset = Event.objects.all()
 
