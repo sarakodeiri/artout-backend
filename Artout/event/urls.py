a5f8b4c45f655eec936ec40b130640c58403aa1d
Sara Kodeiri <skaordaeiri@gmail.com>
2019-12-07 17:52:16 +0330

    Refactor update method for EventDetail

--- Artout/event/urls.py
+++ Artout/event/urls.py
@@ -1,9 +1,9 @@
-from .views import EventList, EventCheckin, EventDetail, EventCheckinDetail
+from .views import EventList, EventCheckinList, EventDetail, EventCheckinDetail
 from django.urls import path
 
 urlpatterns = [
-    path('<int:id>/', EventDetail.as_view()),
+    path('<int:eid>/', EventDetail.as_view()),
     path('/', EventList.as_view()),
-    path('<int:id>/checkins/', EventCheckin.as_view()),
-    path('<int:id>/checkins/<int:cd>', EventCheckinDetail.as_view())
+    path('<int:eid>/checkins/', EventCheckinList.as_view()),
+    path('<int:eid>/checkins/<int:cid>', EventCheckinDetail.as_view())
 ]
--- Artout/event/views.py
+++ Artout/event/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import EventSerializer, CheckinSerializer
 from .models import Event, UserProfile, CheckIn
+from django.http import HttpResponseForbidden
+
 from django.core.exceptions import *
 from django.http import JsonResponse, HttpResponseBadRequest
 
@@ -14,8 +16,8 @@ class EventList(generics.ListCreateAPIView):
     serializer_class = EventSerializer
 
     def get_queryset(self):
-        user = get_object_or_404(UserProfile, pk=self.kwargs['owner'])
-        return Event.objects.filter(owner__id=user.id)
+        owner_id = self.request.query_params.get('owner')
+        return Event.objects.filter(owner_id=owner_id)
 
     def create(self, request, *args, **kwargs):
         data = request.data
@@ -28,10 +30,23 @@ class EventList(generics.ListCreateAPIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
-class EventDetail(generics.RetrieveUpdateAPIView):
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated,)
-    queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        return Event.objects.filter(id=self.kwargs['eid'])
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        if request.user == instance.owner:
+            return HttpResponseForbidden()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
 
 
 class EventCheckinList(generics.ListCreateAPIView):
