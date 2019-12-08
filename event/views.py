d1b2e952e35e248baf8e6d9130c37dd3092a6105
Sara Kodeiri <skaordaeiri@gmail.com>
2019-12-08 13:24:57 +0330

    Retreive previously implemented views in event view

--- event/views.py
+++ event/views.py
@@ -1,48 +1,82 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import EventSerializer, CheckinSerializer
 from .models import Event, UserProfile, CheckIn
-from django.core.exceptions import *
-from django.http import JsonResponse, HttpResponseBadRequest
+from django.http import HttpResponseForbidden
 
 
 class EventList(generics.ListCreateAPIView):
-    permission_classes = (AllowAny,)
-    queryset = Event.objects.all()
+    permission_classes = (IsAuthenticated,)
     serializer_class = EventSerializer
 
     def get_queryset(self):
-        user = get_object_or_404(UserProfile, pk=self.kwargs['id'])
-        return Event.objects.filter(owner__id=user.id)
+        owner_id = self.request.query_params.get('owner')
+        return Event.objects.filter(owner_id=owner_id)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        user = request.user
+        data["owner"] = user
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
-class EventDetail(generics.RetrieveUpdateAPIView):
-    permission_classes = (AllowAny,)
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = EventSerializer
-    queryset = Event.objects.all()
+
+    def get_queryset(self):
+        return Event.objects.filter(id=self.kwargs['eid'])
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        if request.user != instance.owner:
+            return HttpResponseForbidden()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user != instance.owner:
+            return HttpResponseForbidden()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class EventCheckin(generics.CreateAPIView):
-    permission_classes = (AllowAny,)
+class EventCheckinList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = CheckinSerializer
 
-    def post(self, request, *args, **kwargs):
-        user = UserProfile.objects.get(id=request.data['user'])
-        event = Event.objects.get(id=request.data['event'])
-        if CheckIn.objects.filter(event=event, user=user).exists():
-            CheckIn.objects.get(event=event, user=user).delete()
-            return Response('Unchecked', status=status.HTTP_200_OK)
-        else:
-            serializer = self.serializer_class(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response('Checked-in', status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        event_id = self.kwargs.get('eid')
+        event = get_object_or_404(Event, id=event_id)
+        return CheckIn.objects.filter(event=event)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        user = request.user
+        data["user"] = user
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class EventCheckinDetail(generics.DestroyAPIView):
-    pass
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user != instance.owner:
+            return HttpResponseForbidden()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
