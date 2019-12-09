f39b77ae7d1a7c3bc24773b882c758d1780d1300
Sara Kodeiri <skaordaeiri@gmail.com>
2019-12-09 08:58:18 +0330

    Rename authserializer to auth_serializer and userserializer to user_serializer

--- authentication/views.py
+++ authentication/views.py
@@ -5,8 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from user.models import UserProfile
-from . import serializers as authserializers
-from user import serializers as userserializers
+from . import serializers as auth_serializers
+from user import serializers as user_serializers
 
 
 class RegisterView(generics.CreateAPIView):
@@ -14,14 +14,14 @@ class RegisterView(generics.CreateAPIView):
         Register a new user with the user data
     """
     permission_classes = (AllowAny,)
-    serializer_class = authserializers.RegisterSerializer
+    serializer_class = auth_serializers.RegisterSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
             user = serializer.save()
-            serializer_user = userserializers.UserSerializer(user)
+            serializer_user = user_serializers.UserSerializer(user)
             tokens = serializer.data['tokens']
             data = {"refresh": tokens["refresh"], "access": tokens["access"], "id": serializer_user.data["id"]}
             return Response(data, status=status.HTTP_201_CREATED)
@@ -32,7 +32,7 @@ class LoginView(TokenObtainPairView):
     """
         Get Refresh And Access Token by Username and Password Given
     """
-    serializer_class = authserializers.LoginSerializer
+    serializer_class = auth_serializers.LoginSerializer
     permission_classes = (AllowAny,)
 
     def post(self, request, *args, **kwargs):
