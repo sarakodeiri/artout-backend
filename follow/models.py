063f0cd6bdb77c65d33590f5a2d1a1d2a6b8ed9e
Mohammad Yahyavi <yahyavimohammad@gmail.com>
2019-12-11 19:18:03 +0330

    Add FollowRequest model

--- follow/models.py
+++ follow/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from user.models import UserProfile
+
+
+class FollowRequest(models.Model):
+    from_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="follow_pending")
+    to_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="follow_request")
+    created = models.DateTimeField(auto_now_add=True)
+
+
