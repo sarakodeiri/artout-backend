d2430fa1df9b7d9eebf5e6a178a3ab45f14454d7
Mohammad Yahyavi <yahyavimohammad@gmail.com>
2019-12-11 19:47:54 +0330

    Add accept, delete, reject methods for FollowRequest

--- follow/models.py
+++ follow/models.py
@@ -8,6 +8,19 @@ class FollowRequest(models.Model):
     to_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="follow_request")
     created = models.DateTimeField(auto_now_add=True)
 
+    def accept(self):
+        follow = Follow.objects.create(follower=self.from_user, followee=self.to_user)
+        self.delete()
+        return True
+
+    def reject(self):
+        self.delete()
+        return True
+
+    def cancel(self):
+        self.delete()
+        return True
+
 
 class Follow(models.Model):
     follower = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="following")
