d294c129aa764648b71a85b7037df85dbc205171
Mohammad Yahyavi <yahyavimohammad@gmail.com>
2019-12-12 01:03:16 +0330

    Add a serializer to partially serialize user for following/follower list apis

--- user/serializers.py
+++ user/serializers.py
@@ -7,3 +7,9 @@ class UserSerializer(serializers.ModelSerializer):
         model = models.UserProfile
         fields = ('id', 'email', 'first_name', 'last_name', 'date_joined', 'avatar', 'username', 'is_private')
         read_only_fields = ('is_private',)
+
+
+class UserPreviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = models.UserProfile
+        fields = ('id', 'fist_name', 'last_name', 'username')
