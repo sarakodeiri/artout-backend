f09c945da2554be287bef73adcca41d5474911e4
Mohammad Yahyavi <yahyavimohammad@gmail.com>
2019-12-13 00:23:46 +0330

    Add FollowersDetail view and url

--- follow/views.py
+++ follow/views.py
@@ -49,3 +49,19 @@ class FollowingsDetail(generics.RetrieveDestroyAPIView):
         obj = get_object_or_404(queryset, pk=self.kwargs['uid'])
         self.check_object_permissions(self.request, obj)
         return obj
+
+
+class FollowersDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = user_serializers.UserPreviewSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return follow_models.Follow.objects.followers(user)
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = get_object_or_404(queryset, pk=self.kwargs['uid'])
+        self.check_object_permissions(self.request, obj)
+        return obj
+
