4d43f99fda7b176f9d062856a3cf0a11d57f0d81
Mohammad Yahyavi <yahyavimohammad@gmail.com>
2019-12-13 00:08:06 +0330

    Add FollowingsList view and url

--- follow/views.py
+++ follow/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from follow import models as follow_models
+from user import serializers as user_serializers
+from user import models as user_models
+
+
+class FollowingsList(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = user_serializers.UserPreviewSerializer
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user')
+        if user_id is None:
+            user = self.request.user
+            return follow_models.Follow.objects.followings(user)
+        else:
+            user = get_object_or_404(user_models.UserProfile, id=user_id)
+            return follow_models.Follow.objects.followings(user)
