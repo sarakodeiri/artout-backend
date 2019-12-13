70208316ab015fa673a2070435e6e9b910ca2782
Mohammad Yahyavi <yahyavimohammad@gmail.com>
2019-12-13 18:46:08 +0330

    Modify imports in follow views

--- follow/views.py
+++ follow/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from follow import models as follow_models
+from . import models as follow_models
 from user import serializers as user_serializers
 from user import models as user_models
 
