33f3a1dec4fc8ff6e08381736f77b68b09931365
Sara Kodeiri <skaordaeiri@gmail.com>
2019-12-16 21:56:56 +0330

    Remove imports

--- user/views.py
+++ user/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import get_object_or_404
-
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
+
 
 from user.models import UserProfile
 from user import serializers
