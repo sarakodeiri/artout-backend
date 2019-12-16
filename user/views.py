e359b3187ad99c8376876937dec6ed47ac8c064b
Sara Kodeiri <skaordaeiri@gmail.com>
2019-12-16 20:00:29 +0330

    Implement get_object in user views

--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 from user.models import UserProfile
 from user import serializers
@@ -10,4 +12,16 @@ class UserList(generics.ListAPIView):
     serializer_class = serializers.UserSerializer
     queryset = UserProfile.objects.all()
 
+
 class UserDetail(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.UserSerializer
+    queryset = UserProfile.objects.all()
+
+    def get_object(self):
+        try:
+            id = UserProfile.objects.get(username=self.kwargs['un'])
+            return Response(id, status=status.HTTP_200_OK)
+        except id.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
