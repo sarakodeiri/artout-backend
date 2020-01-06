dc6b3dbc8d5a8cde97192cf52665cb2f69f1e8df
Sara Kodeiri <skaordaeiri@gmail.com>
2020-01-06 16:26:33 +0330

    Migrate new checkin app

--- checkin/models.py
+++ checkin/models.py
@@ -5,7 +5,7 @@ from user.models import UserProfile
 
 
 class CheckIn(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="checkins")
+    user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, related_name="checkins")
     submitted_time = models.DateTimeField(auto_now_add=True)
     go_time = models.DateTimeField()
