ac83391d99d3ed7f4a631c08d44c09867e6ec645
Sara Kodeiri <skaordaeiri@gmail.com>
2020-01-06 15:36:29 +0330

    Move checkin views to checkin app

--- checkin/views.py
+++ checkin/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404
+from url_filter.integrations.drf import DjangoFilterBackend
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+from . import models
+
+
+class EventCheckinList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.CheckinSerializer
+
+    def get_queryset(self):
+        event_id = self.kwargs.get('eid')
+        event = get_object_or_404(models.Event, id=event_id)
+        return models.CheckIn.objects.filter(event=event)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        user = request.user
+        data["user"] = user.id
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class EventCheckinDetail(generics.DestroyAPIView):
+    queryset = models.CheckIn.objects.all()
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = get_object_or_404(queryset, pk=self.kwargs['cid'])
+        if self.request.user != obj.owner:
+            return HttpResponseForbidden()
+        self.check_object_permissions(self.request, obj)
+        return obj
--- event/views.py
+++ event/views.py
@@ -73,35 +73,3 @@ class EventDetail(generics.RetrieveUpdateDestroyAPIView):
             return HttpResponseForbidden()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class EventCheckinList(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = serializers.CheckinSerializer
-
-    def get_queryset(self):
-        event_id = self.kwargs.get('eid')
-        event = get_object_or_404(models.Event, id=event_id)
-        return models.CheckIn.objects.filter(event=event)
-
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        user = request.user
-        data["user"] = user.id
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-
-class EventCheckinDetail(generics.DestroyAPIView):
-    queryset = models.CheckIn.objects.all()
-
-    def get_object(self):
-        queryset = self.filter_queryset(self.get_queryset())
-        obj = get_object_or_404(queryset, pk=self.kwargs['cid'])
-        if self.request.user != obj.owner:
-            return HttpResponseForbidden()
-        self.check_object_permissions(self.request, obj)
-        return obj
