66b7c495665bb9584641cb792bb923638d714157
Sara Kodeiri <skaordaeiri@gmail.com>
2020-01-06 18:37:07 +0330

    Implement checkin serializer

--- checkin/serializers.py
+++ checkin/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from . import models
+from user import serializers as user_serializers
+from event import serializers as event_serializers
 
 
 class CheckinSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    event = serializers.SerializerMethodField()
+
     class Meta:
         model = models.CheckIn
-        fields = '__all__'
+        fields = ['event', 'user', 'submitted_time', 'go_time']
+
+    def get_user(self, obj):
+        user_serializer = user_serializers.UserPreviewSerializer(obj.user)
+        return user_serializer.data
+
+    def get_event(self, obj):
+        event_serializer = event_serializers.EventSerializer(obj.event)
+        return event_serializer.data
