a0370cb48a8c0d658ae3a32abf18ffa434d9400d
Mohammad Yahyavi <yahyavimohammad@gmail.com>
2020-01-09 01:11:12 +0330

    Add Timeline to event urls

--- event/urls.py
+++ event/urls.py
@@ -1,7 +1,8 @@
-from .views import EventList, EventDetail
+from .views import EventList, EventDetail, Timeline
 from django.urls import path
 
 urlpatterns = [
     path('<int:eid>/', EventDetail.as_view()),
     path('', EventList.as_view()),
+    path('timeline/', Timeline.as_view())
 ]
