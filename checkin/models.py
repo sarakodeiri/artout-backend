66f08e055cdeed8d76acc5e305bf760205789b44
Mohammad Yahyavi <yahyavimohammad@gmail.com>
2020-01-09 02:05:46 +0330

    Modify how event is passed to checkin

--- checkin/models.py
+++ checkin/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-from event.models import Event
 from user.models import UserProfile
 
 
 class CheckIn(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="checkins")
+    event = models.ForeignKey('event.Event', on_delete=models.CASCADE, related_name="checkins")
     user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, related_name="checkins")
     submitted_time = models.DateTimeField(auto_now_add=True)
     go_time = models.DateTimeField()
